b546d5971e2b8d6822f9878014be1910e9eaf7f3
ayush010898 <44065684+ayush010898@users.noreply.github.com>
2021-08-23 16:27:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express()
 app.use(express.urlencoded({extended:true}))  //use content form body directly
 //app.use(express.static("public"))
 app.use(express.json())
-var server = app.listen(8089,function() {
+var server = app.listen(8085,function() {
     console.log("Server started")
 })
 
